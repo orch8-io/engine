e81d57cf1012b2ed6e7fddda3dbf3557906a4bc1
Oleksii <lxvslnk@gmail.com>
2026-04-15 16:08:57 -0300

    feat: add WorkerTaskFilter type and storage trait methods
    Add WorkerTaskFilter and WorkerTaskStats types for querying worker
    tasks with filtering/pagination. Extend StorageBackend trait with
    list_worker_tasks and worker_task_stats methods.

---
+++ orch8-types/src/worker_filter.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+use crate::worker::WorkerTaskState;
+
+#[derive(Debug, Clone, Default, Deserialize)]
+pub struct WorkerTaskFilter {
+    pub states: Option<Vec<WorkerTaskState>>,
+    pub handler_name: Option<String>,
+    pub worker_id: Option<String>,
+    pub queue_name: Option<String>,
+}
+
+#[derive(Debug, Clone, Default, Serialize)]
+pub struct WorkerTaskStats {
+    pub by_state: HashMap<String, u64>,
+    pub by_handler: HashMap<String, HashMap<String, u64>>,
+    pub active_workers: Vec<String>,
+}
